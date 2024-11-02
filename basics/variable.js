2fdf9049065b5902ea819b8ade023023ab6e77f7
3shivamraj <141985652+3shivamraj@users.noreply.github.com>
2024-11-02 13:27:26 +0000

    2nd day

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const id = 1234
+let email = "shivam@gmail.com"
+var password= 1234
+city = "patna"
+let area;
+email = "shivam@gmail.bawal"
+console.log(email)
+console.table([id,email,area,password,city])
+const students = [
+    { name: "Alice", age: 20 },
+    { name: "Bob", age: 22 }
+  ];
+  console.table(students); // Displays data in a table
+  
